16223aa13137ed1a9112c742d50fd0a43a956421
Akanksha Patil <akankshapatil3024@gmail.com>
2026-07-15 12:36:22 +0530

    add code

---
+++ 10_Pointers.c/%d.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 20 ;
+    int *ptr = &age ;
+
+    printf("%d\n",&age);
+    printf("%d\n",ptr);
+    printf("%d\n",*ptr);
+    printf("%d\n",*ptr);
+    printf("%d\n",&ptr);
+    printf("%d\n",&ptr);
+
+
+    return 0;
+}
