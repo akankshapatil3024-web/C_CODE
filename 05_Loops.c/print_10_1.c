2eac93fb3e59c0ecc304df31d33b3e1985299a13
Akanksha Patil <akankshapatil3024@gmail.com>
2026-07-20 11:50:09 +0530

    add c

---
+++ 05_Loops.c/print_10_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n , i ;
+
+    printf("enter n :");
+    scanf("%d", &n);
+
+    for (int i = n; i >= 1; i--)
+    {
+       printf("%d\n",i);
+    }
+    
+    return 0;
+}
