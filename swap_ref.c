b981fb8ca702122d98f9b3aa1f6b24ad4c4063c7
Akanksha Patil <akankshapatil3024@gmail.com>
2026-07-22 13:09:20 +0530

    add c codes

---
+++ swap_ref.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void swap(int *a , int *b);
+int main()
+{
+    int x = 4 , y = 5 ;
+    printf("x = %d & y = %d\n",x,y);
+    swap(&x,&y);
+    return 0;
+}
+void swap(int *a , int *b){
+    int t = *a ;
+    *a = *b;
+    *b = t ;
+    printf("a = %d & b = %d\n",a,b);
+}
